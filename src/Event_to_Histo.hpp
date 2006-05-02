39865fe7a1b7194aa16efb38a71b4984c3ce1eef
Jean Bilheux <bilheuxjm@ornl.gov>
2006-05-02 17:27:55 +0000

    Header file that contains the declaration of the function used in the .cpp file. Also, set the propset on this new file. Please verify #15

---
+++ src/Event_to_Histo.hpp
@@ -0,0 +1,46 @@
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <sys/stat.h>
+#include <vector>
+#include <time.h>
+
+// $Id$
+
+//to swap from little to big endian
+inline void endian_swap(binary_type & x);  
+
+void SwapEndian (int file_size, 
+                 binary_type * BinaryArray);
+
+// Initialize Array
+void InitializeArray(binary_type  * data_histo, 
+                     int Nx, 
+                     int Ny,
+                     int new_nt);
+
+// Generate the name of the output file
+void produce_output_file_name(string & file_name,
+                              string & output_file,
+                              string & path);
+
+// Generate histogram
+void Generate_data_histo(binary_type * data_histo,
+                         binary_type * BinaryArray, 
+                         int GlobalArraySize, 
+                         char type_of_rebining,
+                         float rebin_value,
+                         int new_Nt);
+
+// Print the help menu
+void print_help();
+
+// Get the rebin value
+float get_rebin_value(char *argv[]);
+
+// Isolate file_name from path+file_name
+void isolate_file_name(string & path_file_name,
+                       string & file_name,
+                       string & path);
