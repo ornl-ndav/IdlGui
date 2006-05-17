e2416a64b159ed3ea89488a8f5515ac9520a8d39
2zr <2zr@93334afa-840f-0410-a8fc-81e7bb264a29>
2006-05-17 16:04:52 +0000

    Moving function implementation from hpp to cpp. This refs #42.

--- trunk/cli/Map_Data.cpp
+++ trunk/cli/Map_Data.cpp
@@ -33,6 +33,129 @@
 using namespace std;
 using namespace TCLAP;
 
+const std::string make_mapped_filename(const std::string full_path,
+                                       const std::string alt_path,
+                                       bool debug)
+{
+  std::string outfile("");
+  if(alt_path != "")
+    {
+      std::string file = full_path.substr(full_path.rfind('/')+1);
+      outfile.append(alt_path);
+      outfile.append("/");
+      outfile.append(file);
+    }
+  else
+    {
+      outfile.append(full_path);
+    }
+
+  outfile.insert(outfile.rfind('.'), MAP_FILE_POSTFIX);
+
+  if(debug)
+    {
+      std::cout << "Output File = " << outfile << std::endl;
+    }
+  
+  return outfile;
+}
+
+void make_pixel_map(const std::string mapfile, 
+                    const int32_t num_pixels, 
+                    std::map<int32_t, int32_t> & pixel_map,
+                    bool debug)
+{
+  // Read in the mapping file
+  std::ifstream m_data(mapfile.c_str(), std::ios::binary);
+  if(!m_data.is_open())
+    {
+      throw std::runtime_error("Failed opening mapping file");
+    }
+
+  int32_t pm_buffer[num_pixels];
+  m_data.read(reinterpret_cast<char *>(pm_buffer), 
+              num_pixels*sizeof(pm_buffer));
+  m_data.close();
+  
+  // Create the pixel map
+  for(size_t i = 0; i < num_pixels; ++i)
+    {
+      pixel_map[pm_buffer[i]] = i;
+    }
+  
+  if(debug)
+    {
+      std::cout << "Pixel Map:" << std::endl;
+      std::map<int32_t, int32_t>::iterator iter;
+      for(iter = pixel_map.begin(); iter != pixel_map.end(); ++iter)
+        {
+          std::cout << iter->first << "\t" << iter->second << std::endl;
+        }
+    }
+}
+
+void print_data_block(const int32_t size, const int32_t *block)
+{
+  for(size_t i = 0; i < size; ++i)
+    {
+      std::cout << i << "\t" << block[i] << std::endl;
+    }
+
+  return;
+}
+
+void create_mapped_data(const std::string neutronfile, 
+                        const std::string mappedfile,
+                        const int32_t num_tof_bins,
+                        const std::map<int32_t, int32_t> & pixel_map,
+                        bool debug)
+{
+  // Open binary data file
+  std::ifstream neutron_data(neutronfile.c_str(), std::ios::binary);
+  if(!neutron_data.is_open())
+    {
+      throw std::runtime_error("Failed opening original binary file");
+    }
+
+  // Open mapped binary data file
+  std::ofstream mapped_data(mappedfile.c_str(), std::ios::binary);
+  if(!mapped_data.is_open())
+    {
+      throw std::runtime_error("Failed opening mapped binary file");
+    }
+  
+  int32_t data_buffer[num_tof_bins];
+  
+  // Iterate over the pixel map
+  std::map<int32_t, int32_t>::const_iterator iter;
+  for(iter = pixel_map.begin(); iter != pixel_map.end(); ++iter)
+    {
+      if(debug)
+        {
+          std::cout << "Position: " << iter->second << std::endl;
+          std::cout << "Offset  : " ;
+          std::cout << iter->second * num_tof_bins * SIZEOF_INT32_T;
+          std::cout << std::endl;
+        }
+      
+      // Move to proper read location
+      neutron_data.seekg(iter->second*num_tof_bins*SIZEOF_INT32_T, 
+                         std::ios::beg);
+      // Read data from file
+      neutron_data.read(reinterpret_cast<char *>(data_buffer), 
+                        num_tof_bins*SIZEOF_INT32_T);
+      
+      if(debug)
+        {
+          print_data_block(num_tof_bins, data_buffer);
+        }
+      
+      // Write data to mapped file
+      mapped_data.write(reinterpret_cast<char *>(data_buffer), 
+                        num_tof_bins*SIZEOF_INT32_T);
+    }
+}
+
 /**
  * \brief This program takes a binary data file, a mapping file and creates 
  *        a mapped binary data file.
