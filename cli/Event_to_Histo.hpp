2bf9d153961fab7d5273f881d0b2d78cbea25e4a
Jean Bilheux <bilheuxjm@ornl.gov>
2006-05-31 13:45:13 +0000

    New version of header file that goes with the new Event_to_Histo_cli file. this refs #44

--- cli/Event_to_Histo.hpp
+++ cli/Event_to_Histo.hpp
@@ -1,13 +1,47 @@
+/*
+ *                       Histogramming Tools
+ *           A part of the SNS Analysis Software Suite.
+ *
+ *                  Spallation Neutron Source
+ *          Oak Ridge National Laboratory, Oak Ridge TN.
+ *
+ *
+ *                             NOTICE
+ *
+ * For this software and its associated documentation, permission is granted
+ * to reproduce, prepare derivative works, and distribute copies to the public
+ * for any purpose and without fee.
+ *
+ * This material was prepared as an account of work sponsored by an agency of
+ * the United States Government.  Neither the United States Government nor the
+ * United States Department of Energy, nor any of their employees, makes any
+ * warranty, express or implied, or assumes any legal liability or
+ * responsibility for the accuracy, completeness, or usefulness of any
+ * information, apparatus, product, or process disclosed, or represents that
+ * its use would not infringe privately owned rights.
+ *
+ */
+
+/**
+ * $Id: Event_to_Histo.hpp 27 2006-05-02 17:44:39Z j35 $
+ *
+ * \file Event_to_Histo.hpp
+ */
+
+#ifndef _EVENT_TO_HISTO_HPP
+#define _EVENT_TO_HISTO_HPP 1
+
 #include <cmath>
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <stdexcept>
 #include <string>
 #include <sys/stat.h>
+#include <tclap/CmdLine.h>
 #include <vector>
 #include <time.h>
-
-// $Id: Event_to_Histo.hpp 27 2006-05-02 17:44:39Z j35 $
+#include <stdint.h>
 
 typedef int binary_type;
 
@@ -49,3 +83,6 @@ float get_rebin_value(char *argv[]);
 void isolate_file_name(string & path_file_name,
                        string & file_name,
                        string & path);
+
+
+#endif // _EVENT_TO_HISTO_HPP
