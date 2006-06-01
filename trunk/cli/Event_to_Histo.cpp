fe12d510373633bda6e500f2988719e417ea6ada
j35 <j35@93334afa-840f-0410-a8fc-81e7bb264a29>
2006-06-01 17:27:34 +0000

    New version of Event_to_Histo.cpp/hpp files: 	- added command line that takes several arguments (alternate output file name, debug flag, input file name, Nt of event file, number of pixels, new time bin, linear flag for linear rebinning (the only one supported for this version), number of lines to print out for debugging purpose, possibility to swap endian ofbinary file and histo array created). This refs #44, #14, #13. 	- Changed the way the output file name is produced. This refs #39, #40. 	- type of data is not int anymore but int32_t. This refs #27. 	- Exceptions do ot escape main anymore. This refs #25. 	- Remove dead code. This refs #23. 	- Convert all character arrays to strings. This refs #22. 	- Added const keyword in functions as appropriate. This refs #21. 	- Removed get_rebin_value. This refs #18. 	- Changed to isolate_file_name. This refs #17.

--- trunk/cli/Event_to_Histo.cpp
+++ trunk/cli/Event_to_Histo.cpp
@@ -1,126 +1,191 @@
 #include "Event_to_Histo.hpp"
 
-// $Id: Event_to_Histo.cpp 28 2006-05-02 17:45:45Z j35 $
+/*
+ * $Id: Event_to_Histo.cpp 28 2006-05-02 17:45:45Z j35 $
+ */
 
-using std::cout;
-using std::endl;
+using namespace std;
+using namespace TCLAP;
 
 int main(int argc, char *argv[])
 {
-  struct stat results;
-
-  int file_size;
-  int pixelID;
-  int time_stamp;
-  int Histo_size;
-  int GlobalArraySize;
-  int Nx = 256;  //Nx of the histo array
-  int Ny = 304; //Ny of the histo array
-  int Nt = 167;  //Number of time bin of the histo array
-  int new_Nt;
-  
-  float rebin_value;
-  
-  char type_of_rebining;
-
-  // Name of file
-  string path_file_name;
-  string file_name;
-  string output_file_name;
-  string path;
-
-  if (argc>1)  //if there is at least one argument
+  try
     {
-      if (argv[1][0]=='-' && argv[1][1]=='-' && argv[1][2]=='h')
+      // Setup the command-line parser object
+      CmdLine cmd("Command line description message", ' ', VERSION_TAG);
+
+      // Add command-line options
+      ValueArg<string> altoutpath("a", "alternate_output", 
+                                  "Alternate path for output file",
+                                  false, "", "path", cmd);
+
+      SwitchArg debugSwitch("d", "debug", "Flag for debugging program",
+                            false, cmd);
+
+      SwitchArg swapiSwitch ("i", "swap_input", 
+                            "Flag for swapping data of input file",
+                            false, cmd);
+      
+      SwitchArg swapoSwitch ("o", "swap_output",
+                             "Flag for swapping data of output file",
+                             false, cmd);
+
+      ValueArg<int32_t> pixelnumber ("p", "number_of_pixels",
+                                     "Number of pixels for this run",
+                                     true, -1, "pixel number", cmd);
+
+      ValueArg<int32_t> timebin("t", "time_bin", 
+                                "Number of time bin in event file",
+                                true, -1, "time bin number", cmd);
+
+      ValueArg<int32_t> timerebin("l","linear",
+                                   "size of rebin linear time bin",
+                                   true, -1, "new linear time bin", cmd);
+
+      ValueArg<string> event_file("f", "event", "Name of the event file", 
+                              true, "event.dat", "filename", cmd);
+
+      ValueArg<int32_t> n_values("n", "output_value",
+                                 "number of values of files to print out",
+                                 false, 5, "values to output", cmd);
+
+      // Parse the command-line
+      cmd.parse(argc, argv);
+      
+      // isolate path from file name and create output file name
+      string input_filename;
+      string output_filename("");
+      string path; 
+      string input_file = event_file.getValue();
+      bool debug = debugSwitch.getValue();
+      
+      path_input_output_file_names(input_file,
+                                   input_filename,
+                                   path,
+                                   altoutpath.getValue(),
+                                   output_filename,
+                                   debug);
+
+      // read event binary array
+      FILE *e_file;
+      e_file = fopen(input_file.c_str(), "rb");
+
+      struct stat results;
+
+      if (!stat(input_file.c_str(), &results)==0)
         {
-          print_help();
-          return 0;
+          throw runtime_error("Failed to determine size of event binary file");
         }
-      else
+      
+      int file_size = results.st_size/sizeof(int32_t);
+
+      // allocate memory for the binary array
+      int32_t * binary_array = new int32_t [file_size];
+      
+      // transfer the data from the event binary file into binary_array
+      fread(&binary_array[0], 
+            sizeof(binary_array[0]),
+            file_size,
+            e_file);
+
+      // displays the n_disp first values of the event binary file
+      int n_disp = n_values.getValue();
+      string message;
+      if(debug)
         {
-          type_of_rebining = argv[2][1];
-          rebin_value = get_rebin_value(argv);
+          if (swapiSwitch.getValue())
+            {
+              message = "\nBefore swapping the data";
+            }
+          else
+            {
+              message = n_disp + " first values of " + input_filename +"\n"; 
+            }
+          print_n_first_data(binary_array, 
+                             n_disp,
+                             message);
         }
+
+      if (swapiSwitch.getValue())
+        {
+          swap_endian(file_size, binary_array);
+        }
+
+      if(debug)
+        {
+          if (swapiSwitch.getValue())
+            {
+              message = "\nAfter swapping the data";
+            }
+          else
+            {
+              message = n_disp + " first values of " + input_filename +"\n"; 
+            }
+          print_n_first_data(binary_array, 
+                             n_disp,
+                             message);
+        }
+
+      // allocate memory for the histo array
+      int32_t time_bin = timebin.getValue();
+      int32_t time_rebin = timerebin.getValue();
+      int32_t pixel_number = pixelnumber.getValue();
+
+      //initialize array
+      int new_Nt = int(floor((time_bin*100)/time_rebin));
+      int32_t histo_array_size = new_Nt * pixel_number;
+      int32_t * histo_array = new int32_t [histo_array_size];
+
+      initialize_array(histo_array,
+                       histo_array_size);
+      
+      // and create histo binary data array
+      generate_histo(file_size,
+                     new_Nt,
+                     pixelnumber.getValue(),
+                     time_rebin,
+                     binary_array,
+                     histo_array,
+                     debug);
+
+      if(debug)
+        {
+          message = "\nhisto_array";
+          print_n_first_data(histo_array, 
+                             n_disp,
+                             message);
+        }
+
+      if (swapoSwitch.getValue())
+        {
+          swap_endian(histo_array_size, histo_array);
+        }
+
+      // write new histogram file
+      std::ofstream histo_file(output_filename.c_str(),
+                               std::ios::binary);
+      histo_file.write((char*)(histo_array),
+                       sizeof(histo_array)*histo_array_size);
+      histo_file.close();
+
     }
-
-  path_file_name = argv[1];
-  isolate_file_name(path_file_name,file_name,path);
-
-  produce_output_file_name(file_name,output_file_name, path);
-
-  // Open and Read binary file
-  FILE *BinaryFile;
-  BinaryFile=fopen(path_file_name.c_str(),"rb");
-  
-  // Get the size of the binary file
-  if (stat(path_file_name.c_str(),&results)==0)
+  catch (ArgException &e)
     {
-      file_size = results.st_size/sizeof(binary_type);
-    }
-  else
-    throw std::logic_error("Can't determine the size of the file");
-  
-  // check that open was successful
-  if (BinaryFile==NULL)
-    throw std::invalid_argument("Could not open file: " + file_name);
-  
-  //allocate memory for the binary Array
-  GlobalArraySize = file_size;
-  binary_type * BinaryArray = new binary_type [GlobalArraySize];
-  
-  //transfer the data from the binary file into the GlobalArray
-  fread(&BinaryArray[0],sizeof(BinaryArray[0]), GlobalArraySize, BinaryFile);
-  
-  // swap endian (PC <-> Mac)
-  SwapEndian (file_size, BinaryArray);
-
-  switch (type_of_rebining)
-    {
-    case 'l':
-      new_Nt = int(floor((Nt*100)/rebin_value)); //rebin_value=200=>new_Nt=83
-      break;
-    case 'd':
-      break;
-    case 'L':
-      break;
+      cerr << "Error: " << e.error() << " for arg " << e.argId() << endl;
     }
 
-  Histo_size = Nx*Ny*new_Nt;
-  binary_type *data_histo = new binary_type[Histo_size];
-  
-  //initialize array
-  InitializeArray(data_histo, Nx, Ny, new_Nt);
-  
-  Generate_data_histo(data_histo,
-                      BinaryArray, 
-                      GlobalArraySize, 
-                      type_of_rebining,
-                      rebin_value,
-                      new_Nt);
 
-  for (int i=0; i<GlobalArraySize/2; i++)
-    {
-      pixelID = BinaryArray[2*i+1];
-      time_stamp = int(floor((BinaryArray[2*i]/10)/rebin_value)); 
-      data_histo[time_stamp+pixelID*new_Nt]+=1;      
-    }
-
-  // write new histogram file
-  std::ofstream file(output_file_name.c_str(), std::ios::binary);
-  file.write((char*)(data_histo),sizeof(data_histo)*Histo_size);  
-  file.close();
-  
   return 0;
 }
 
 /*********************************************
 /Loop through BinaryArray to swap all endians
 /********************************************/
-void SwapEndian (int file_size, binary_type * BinaryArray)
+void swap_endian (int file_size, int32_t * BinaryArray)
 {
   for (int j=0; j<file_size; ++j)
     {
-      endian_swap(BinaryArray[j]);
+      swap_digit(BinaryArray[j]);
     }
   
   return;
@@ -129,7 +194,7 @@ void SwapEndian (int file_size, binary_type * BinaryArray)
 /*******************************************
 /To swap from little endian to big endian
 /*******************************************/
-inline void endian_swap (binary_type & x)
+inline void swap_digit (int32_t & x)
 {
   x = ((x>>24) & 0x000000FF) |
     ((x<<8) & 0x00FF0000) |
@@ -140,141 +205,139 @@ inline void endian_swap (binary_type & x)
 /*******************************************
 /Initialize the array
 /*******************************************/
-void InitializeArray(binary_type * data_histo, 
-                     int Nx, 
-                     int Ny,
-                     int new_Nt)
+void initialize_array(int32_t * histo_array, 
+                      const int size)
 {
-  int size = Nx*new_Nt*Ny;
-  
-  for (int a=0; a<size;++a)
+  for (int i=0 ; i<size ; ++i)
     {
-      data_histo[a]=0;
+      histo_array[i]=0;
      }
   
   return;   
 }
 
 /*******************************************
-/Generate the name of the output file
-/*******************************************/
-void produce_output_file_name(string & file_name,
-                              string & output_file,
-                              string & path)
-{
-  int file_name_size = file_name.size();
-  string local_file_name = file_name;
-  int index;
-
-  index = file_name.find("event");
-  
-  output_file = path + local_file_name.substr(0,index) + "histo" + \
-    file_name.substr(index+5,file_name_size);
-
-  return;
-}
-
-/*******************************************
-/Generate the name of the output file
-/*******************************************/
-void Generate_data_histo(binary_type * data_histo,
-                         binary_type * BinaryArray, 
-                         int GlobalArraySize, 
-                         char type_of_rebining,
-                         float rebin_value,
-                         int new_Nt)
-{
-  int pixelID;
-  int time_stamp;
-  switch(type_of_rebining)
-    {
-    case 'l':   
-      for (int i=0; i<GlobalArraySize/2; i++)
-        {
-          //          cout << "i= "<<i<<endl;
-          pixelID = BinaryArray[2*i+1];
-          //cout << "\tBinaryArray["<<2*i+1<<"]= "<<BinaryArray[2*i+1]<<endl;
-          time_stamp = int(floor((BinaryArray[2*i]/10)/rebin_value));
-          //cout << "\ttime_stamp= " << time_stamp<<endl;
-          data_histo[time_stamp+pixelID*new_Nt]+=1;      
-          //cout << "\ttime_stamp+pixelID*new_Nt= " <<time_stamp+pixelID*new_Nt<<endl;
-        }
-      break;
-    case 'd':
-      break;
-    case 'L':
-      break;
-    default:
-      cout << "type of rebining not defined"<<endl;
-    }
-     
-  return;
-}
-
-/*******************************************
-/ print the help menu (--help or -h)
-/*******************************************/
-void print_help()
-{
-  cout << "usage: Event_to_Histo input_file_name(relative path) -[ l | b | L]rebin_value"<<endl;
-  cout << "\nArguments:\n\t -l: linear rebining\n\t\tex: \
-Event_to_Histo Input_file_name -l2\n\t\tEach bin will be 2 microseconds \
-wide."<<endl;
-  cout << "\t -d: double rebinning\n\t\tex: Event_to_Histo \
-Input_file_name -d5\n\t\tThe first time bin will be 5 microseconds \
-wide and the second \n\t\ttime bin will contain the rest of the data. "<<endl;
-  cout << "\t -L: logarithmic rebining\n\t\tex: Event_to_Histo\
- Input_file_name -log3.5\n\t\tEach time bin width will be defined by\
- delta(t)/t=3.5"<<endl;
-  cout << endl;
-  cout << "NB: The output file generated will have the same name\
- as the input file with \n'event' replaced by 'histo'"<<endl;
-  return;
-}
-
-/*******************************************
-/ get the rebin value
-/*******************************************/
-float get_rebin_value(char *argv[])
-{
-  string my_string = argv[2];
-  float rebin_value;
-
-  my_string = my_string.substr(2,5); 
-  rebin_value = atof(my_string.c_str());
-
-  return rebin_value;
-}
-
-/*******************************************
 / Isolate file_name from path+file_name
+/ and generate output file name
 /*******************************************/
-void isolate_file_name(string & path_file_name,
-                       string & file_name,
-                       string & path)
+void path_input_output_file_names(string & path_filename,
+                                  string & filename,
+                                  string & path,
+                                  string & alternate_path,
+                                  string & output_filename,
+                                  bool debug)
 {
-  string local_file_name = path_file_name;
-  vector<int> index;
-  int i=0;
   
-  index.push_back(local_file_name.find("/",0));
-  if (index[0]<0)
+  // Parse input file name (path + input_file_name)
+  parse_input_file_name(path_filename,
+                        filename,
+                        path,
+                        debug);
+
+  // Produce output file name (path or alternate_path + output_file_name)
+  produce_output_file_name(filename,
+                           path,
+                           alternate_path,
+                           output_filename,
+                           debug);
+
+  return;
+}
+
+// Isolate path and file_name from command line argument
+void parse_input_file_name(string & path_filename,
+                           string & filename,
+                           string & path,
+                           bool debug)
+{
+  filename = path_filename.substr(path_filename.rfind('/')+1);
+  path = path_filename.substr(0,path_filename.rfind('/')+1);
+ 
+  if(debug)
     {
-      path = "./";
-      file_name = path_file_name;
+      cout << "******************************"<<endl;
+      cout << "In parse_input_file_name " << endl;
+      cout << "   path_filename : " << path_filename << endl;
+      cout << "   filename      : " << filename << endl;
+      cout << "   path          : " << path << endl;
+      cout << "******************************" << endl;
+    }
+
+  return;
+}
+
+// Generate the name of the output file
+void produce_output_file_name(string & filename,
+                              string & path,
+                              string & alternate_path,
+                              string & output_filename,
+                              bool debug)
+{
+  string outfile = filename.substr(0,filename.rfind("event"));
+  outfile = outfile.append(HISTO_FILE_TAG);
+  string local_path("");
+
+  if (alternate_path != "")
+    {
+      local_path = alternate_path;
     }
   else
     {
-      while(index[i]>=0)
-        {
-          index.push_back(local_file_name.find("/",index[i]+1));
-          i++;
-        }
+      local_path = path;
     }
+
+  output_filename.append(local_path);
+  output_filename.append("/");
+  output_filename.append(outfile);
   
-  path = path_file_name.substr(0,index[i-1]+1);
-  file_name = path_file_name.substr(index[i-1]+1,path_file_name.size());
-      
+  if (debug)
+    {
+      cout << endl;
+      cout << "*********************************"<<endl;
+      cout << "In produce_output_file_name " << endl;
+      cout << "   outfile         : " << outfile << endl;
+      cout << "   local_path      : " << local_path << endl;
+      cout << "   output_filename : " << output_filename << endl;
+      cout << "*********************************"<<endl;      
+    }
+
   return;
 }
 
+void print_n_first_data(const int32_t * binary_array,
+                        const int n_disp,
+                        const string message)
+{
+  cout << message << endl;
+  for (size_t i=0 ; i<n_disp ; ++i)
+    {
+      cout << "\tdata[" << i << "]= " << binary_array[i]<<endl;
+    }
+  
+  return;
+}
+
+// create histo binary data array
+void generate_histo(const int file_size,
+                    const int new_Nt,
+                    const int32_t pixelnumber,
+                    const int32_t time_rebin,
+                    const int32_t * binary_array,
+                    int32_t * histo_array,
+                    const bool debug)
+{
+  int pixelid;
+  int time_stamp;
+
+  for (size_t i=0 ; i<file_size/2; i++)
+    {
+      pixelid = binary_array[2*i+1];
+      time_stamp = int(floor((binary_array[2*i]/10)/time_rebin));
+      histo_array[time_stamp+pixelid*new_Nt]+=1;
+    }
+  
+  return;
+}
+
+
+                        
