ee2d10544f918f61e1b15bd33c75e45270ac5c20
Jean Bilheux <bilheuxjm@ornl.gov>
2006-06-01 17:27:34 +0000

    New version of Event_to_Histo.cpp/hpp files: 	- added command line that takes several arguments (alternate output file name, debug flag, input file name, Nt of event file, number of pixels, new time bin, linear flag for linear rebinning (the only one supported for this version), number of lines to print out for debugging purpose, possibility to swap endian ofbinary file and histo array created). This refs #44, #14, #13. 	- Changed the way the output file name is produced. This refs #39, #40. 	- type of data is not int anymore but int32_t. This refs #27. 	- Exceptions do ot escape main anymore. This refs #25. 	- Remove dead code. This refs #23. 	- Convert all character arrays to strings. This refs #22. 	- Added const keyword in functions as appropriate. This refs #21. 	- Removed get_rebin_value. This refs #18. 	- Changed to isolate_file_name. This refs #17.

--- cli/Event_to_Histo.hpp
+++ cli/Event_to_Histo.hpp
@@ -43,46 +43,61 @@
 #include <time.h>
 #include <stdint.h>
 
-typedef int binary_type;
+// this is a string that contains the new part of the binary data file
+static const std::string HISTO_FILE_TAG("histo.dat");
+
+// this is a string that contains the software tag version
+static const std::string VERSION_TAG("1.0.0itc1");
+
+// This is a constant to hold the size of an uint32_t
+static const int SIZEOF_INT32_T = sizeof(int32_t);
 
 using std::string;
 using std::vector;
 
-//to swap from little to big endian
-inline void endian_swap(binary_type & x);  
-
-void SwapEndian (int file_size, 
-                 binary_type * BinaryArray);
-
-// Initialize Array
-void InitializeArray(binary_type  * data_histo, 
-                     int Nx, 
-                     int Ny,
-                     int new_nt);
+// Isolate file_name from path+file_name
+void path_input_output_file_names(string & path_file_name,
+                                  string & file_name,
+                                  string & path,
+                                  string & alternate_output_path,
+                                  string & output_file_name,
+                                  bool debug);
 
 // Generate the name of the output file
-void produce_output_file_name(string & file_name,
-                              string & output_file,
-                              string & path);
+void produce_output_file_name(string & filename,
+                              string & path,
+                              string & alternate_path,
+                              string & output_filename,
+                              bool debug);
 
-// Generate histogram
-void Generate_data_histo(binary_type * data_histo,
-                         binary_type * BinaryArray, 
-                         int GlobalArraySize, 
-                         char type_of_rebining,
-                         float rebin_value,
-                         int new_Nt);
+// Isolate path and file_name from command line argument
+void parse_input_file_name(string & path_filename,
+                           string & filename,
+                           string & path,
+                           bool debug);
 
-// Print the help menu
-void print_help();
+//to swap from little to big endian
+inline void swap_digit(int32_t & x);  
 
-// Get the rebin value
-float get_rebin_value(char *argv[]);
+void swap_endian (int file_size, 
+                 int32_t * BinaryArray);
 
-// Isolate file_name from path+file_name
-void isolate_file_name(string & path_file_name,
-                       string & file_name,
-                       string & path);
+// Initialize Array
+void initialize_array(int32_t * data_histo, 
+                      const int size);
 
+// print n first data of event binary file
+void print_n_first_data(const int32_t * binary_array,
+                        const int n_disp,
+                        const string message);
+
+// create histo binary data array
+void generate_histo(const int file_size,
+                    const int new_Nt,
+                    const int32_t pixel_number,
+                    const int32_t time_rebin,
+                    const int32_t * binary_array,
+                    int32_t * histo_array,
+                    const bool debug);
 
 #endif // _EVENT_TO_HISTO_HPP
