766ac1593b37bae8dfda7bef6912e779d05e6f45
Jean Bilheux <bilheuxjm@ornl.gov>
2006-06-06 18:21:29 +0000

    Moved doxygen documentation of utils.hpp into utils.cpp. This resolves #45

--- cli/utils.cpp
+++ cli/utils.cpp
@@ -32,9 +32,15 @@ inline void swap_digit (int32_t & x)
     ((x<<24) & 0xFF000000);
 }
 
-/*************************************************
-/ to plot the n first data of the specified array
-/*************************************************/
+/**
+ * \brief This function displays the n first element of an array
+ *
+ * \param binary_array (INPUT) is the array for which we need to display
+ * the first n elements
+ * \param n_disp (INPUT) is the number of element to display
+ * \param string_message (INPUT) is the message to display before the n_disp
+ * elements
+ */
 void print32_t_n_first_data(const int32_t * binary_array,
                         const int32_t n_disp,
                         const string message)
@@ -48,10 +54,17 @@ void print32_t_n_first_data(const int32_t * binary_array,
   return;
 }
 
-/*******************************************
-/ Isolate file_name from path+file_name
-/ and generate output file name
-/*******************************************/
+/**
+ * \brief This function parse the name of the event binary file and 
+ * parse it to isolate the path from the name of the file
+ * 
+ * \param path_filename (INPUT) is the complete name of the event binary file
+ * \param filename (INPUT) is the name of the file (without the path part)
+ * \param path (INPUT) is the path to the file (without the name part)
+ * \param alternate_path (INPUT) is the alternate path for the output file
+ * \param output_filename (OUTPUT) is the output file name with its path
+ * \param debug (INPUT) is a flag for printing debugging info
+*/
 void path_input_output_file_names(string & path_filename,
                                   string & filename,
                                   string & path,
@@ -76,7 +89,15 @@ void path_input_output_file_names(string & path_filename,
   return;
 }
 
-// Isolate path and file_name from command line argument
+/**
+ * \brief This function isolate the path from the file name
+ *
+ * \param path_filename (INPUT) is the complete name of the input file 
+ * (path + name)
+ * \param filename (OUTPUT) is the name part only of the input file
+ * \param path (OUTPUT) is the path name only of the input file
+ * \param debug (INPUT) is a flag for printing debugging info
+ */
 void parse_input_file_name(string & path_filename,
                            string & filename,
                            string & path,
@@ -98,9 +119,17 @@ void parse_input_file_name(string & path_filename,
   return;
 }
 
-/*******************************************
-/ Generate the name of the output file
-/*******************************************/
+/**
+ * \brief This function produce the complete name of the output file
+ * 
+ * \param filename (INPUT) is the name part only of the input file
+ * \param path (INPUT) is the path part only of the input file
+ * \param alternate_path (INPUT) is the alternate path provided on the 
+ * command line for the output file name
+ * \param output_filename (OUTPUT) is  the complete name (path + name) of the
+ * output file
+ * \param debug (INPUT) is a flag for printing debugging info
+*/
 void produce_output_file_name(string & filename,
                               string & path,
                               string & alternate_path,
@@ -138,9 +167,19 @@ void produce_output_file_name(string & filename,
   return;
 }
 
-/*******************************************
-/ Read event file and create binary_array
-/*******************************************/
+/**
+ * \brief This function read the event binary file and populate the 
+ * binary_array with its values
+ * 
+ * \param input_file (INPUT) is the name of the event binary file
+ * \param input_filename (INPUT) is the complete name of the event binary file
+ * \param swap_input (INPUT) is a flag that trigger the swapping of the data
+ * coming from the event binary file
+ * \param debug (INPUT) is a flag for printing debugging info
+ * \param n_disp (INPUT) is the number of element to display for debugging
+ * purpose only
+ * \param binary_array (OUTPUT) is the array of the event binary data
+ */
 int32_t read_event_file_and_populate_binary_array(const string & input_file,
                                                   const string & 
                                                   input_filename,
