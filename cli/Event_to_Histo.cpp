17cb4cebdf9ee60aee6325d244b9fe73a3d47e33
Jean Bilheux <bilheuxjm@ornl.gov>
2006-06-07 18:29:21 +0000

    Added copyrights to utils.cpp and Event_to_Histo.cpp files.

--- cli/Event_to_Histo.cpp
+++ cli/Event_to_Histo.cpp
@@ -1,3 +1,27 @@
+/*
+ *                       Histogramming Tools
+ *           A part of the SNS Analysis Software Suite.
+ *
+ *                  Spallation Neutron Source
+ *          Oak Ridge National Laboratory, Oak Ridge TN.
+ *
+ *
+ *                             NOTICE
+ *
+ * For this software and its associated documentation, permission is granted
+ * to reproduce, prepare derivative works, and distribute copies to the public
+ * for any purpose and without fee.
+ *
+ * This material was prepared as an account of work sponsored by an agency of
+ * the United States Government.  Neither the United States Government nor the
+ * United States Department of Energy, nor any of their employees, makes any
+ * warranty, express or implied, or assumes any legal liability or
+ * responsibility for the accuracy, completeness, or usefulness of any
+ * information, apparatus, product, or process disclosed, or represents that
+ * its use would not infringe privately owned rights.
+ *
+ */
+
 #include "Event_to_Histo.hpp"
 
 /*
@@ -133,6 +157,7 @@ int32_t main(int32_t argc, char *argv[])
       // Parse the command-line
       cmd.parse(argc, argv);
       
+
       // isolate path from file name and create output file name
       string input_filename;
       string output_filename("");
@@ -148,6 +173,8 @@ int32_t main(int32_t argc, char *argv[])
                                    output_filename,
                                    debug);
 
+
+      // read input file and populate the binary array 
       int32_t file_size;
       int32_t * binary_array;
 
@@ -167,6 +194,7 @@ int32_t main(int32_t argc, char *argv[])
       int32_t histo_array_size = new_Nt * pixel_number;
       int32_t * histo_array = new int32_t [histo_array_size];
       
+
       //generate histo binary data array
       generate_histo(file_size,
                      new_Nt,
@@ -177,14 +205,16 @@ int32_t main(int32_t argc, char *argv[])
                      histo_array_size,
                      debug);
 
+      // swap endian of output array (histo_array)
       if(swapoSwitch.getValue())
         {
           swap_endian(histo_array_size, histo_array);
         }
 
+      // print out value of histo_array into a file (ASCII format)
       if(showDataArg.getValue())
         {
-          
+          //not implemented yet
         }
 
       // write new histogram file
@@ -194,9 +224,9 @@ int32_t main(int32_t argc, char *argv[])
                        SIZEOF_INT32_T*histo_array_size);
       histo_file.close();
 
+      // free memory allocated to arrays
       delete histo_array;
       delete binary_array;
-
     }
   catch (ArgException &e)
     {
