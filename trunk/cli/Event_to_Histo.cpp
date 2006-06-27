7c2148f73170ffc39fb51e4c3d3f2ac3c0b7654f
j35 <j35@93334afa-840f-0410-a8fc-81e7bb264a29>
2006-06-27 18:45:22 +0000

    Removed obsolete rebin_time_bin from main function. This refs #8

--- trunk/cli/Event_to_Histo.cpp
+++ trunk/cli/Event_to_Histo.cpp
@@ -102,7 +102,6 @@ void initialize_array(uint32_t * histo_array,
  * \param file_size (INPUT) is the size of the file to be read
  * \param new_Nt (INPUT) is the new number of time bins
  * \param pixel_number (INPUT) is the number of pixelids
- * \param time_rebin_width (INPUT) is the new time bin width
  * \param binary_array (INPUT) is the array of values coming from the event
  *  binary file
  * \param histo_array (OUTPUT) is the histogram array
@@ -113,7 +112,6 @@ void initialize_array(uint32_t * histo_array,
 void generate_histo(const int32_t file_size,
                     const int32_t new_Nt,
                     const int32_t pixelnumber,
-                    const int32_t time_rebin_width,
                     const int32_t * binary_array,
                     uint32_t * histo_array,
                     const int32_t histo_array_size,
@@ -414,7 +412,6 @@ int32_t main(int32_t argc, char *argv[])
           generate_histo(file_size,
                          new_Nt,
                          pixelnumber.getValue(),
-                         time_rebin_width,
                          binary_array,
                          histo_array,
                          histo_array_size,
