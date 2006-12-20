db168831b509b145208e45e090303ce1bae2190e
Jean Bilheux <bilheuxjm@ornl.gov>
2006-12-20 16:38:43 +0000

    First part of implementation of logarithmic histogramming. This refs #92 and #118

--- cli/utils.hpp
+++ cli/utils.hpp
@@ -71,7 +71,7 @@ namespace EventHisto
    * time bin must be greater different from 0, this number becomes
    * our "virtual zero".
    */
-  static const int32_t SMALLEST_TIME_BIN_100NS = 1;
+  static const int32_t SMALLEST_TIME_BIN_100NS = 100;
   
   /**
    * \brief This function swap endians of an array
