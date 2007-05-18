c6c53a859b1951cd0052c2e402ab0a086a0e0661
Jean Bilheux <bilheuxjm@ornl.gov>
2007-05-18 14:26:03 +0000

    Add multi_threading code of main plot. This refs #166

---
+++ java/sns-itools-core/src/main/java/gov/ornl/sns/iontools/SubmitPlot.java
@@ -0,0 +1,60 @@
+package gov.ornl.sns.iontools;
+
+public class SubmitPlot implements Runnable {
+
+//	private Configuration configuration = null;
+//	private Component parent = null;
+//	public String function = null;
+//	public TisTreeNode tisTree = null;
+//	private String cmd = null;
+	
+//	public SubmitRun(String cmd) {
+//		this.cmd = cmd;
+//	}
+	
+	private String cmd = null;
+	
+	public SubmitPlot(String cmd) {
+		this.cmd = cmd;
+	}
+	
+	public void run() {
+		
+		IonUtils.executeCmd(this.cmd);
+
+		com.rsi.ion.IONVariable myIONresult;
+		myIONresult = IonUtils.queryVariable("result");
+		String[] myResultArray;
+		myResultArray = myIONresult.getStringArray();
+    		
+		//Nexus file found or not 
+		DataReduction.sNexusFound = myResultArray[0];
+		NexusFound foundNexus = new NexusFound(DataReduction.sNexusFound);
+		DataReduction.bFoundNexus = foundNexus.isNexusFound();
+    				
+		//Number of tof 
+		DataReduction.sNtof = myResultArray[1];
+		Float fNtof = new Float(DataReduction.sNtof);
+		ParametersConfiguration.iNtof = fNtof.intValue();
+			    		
+		//name of tmp output file name
+		DataReduction.sTmpOutputFileName = myResultArray[2];
+		CheckGUI.checkGUI(DataReduction.bFoundNexus);
+    	
+		//check if run number is not already part of the data reduction runs
+		UpdateDataReductionRunNumberTextField.updateDataReductionRunNumbers(DataReduction.runNumberValue);
+    
+		//update text field
+		if (DataReduction.bFoundNexus) {
+			//tell the program that it's not the first run ever
+			ParametersToKeep.bFirstRunEver=false;
+			if (CheckDataReductionButtonValidation.bAddNexusAndGo) {
+				DataReduction.runsAddTextField.setText(CheckDataReductionButtonValidation.sAddNexusAndGoString);
+			} else {
+    			DataReduction.runsSequenceTextField.setText(CheckDataReductionButtonValidation.sGoSequentiallyString);
+    		}	
+    	}	
+			
+	}
+	
+}
