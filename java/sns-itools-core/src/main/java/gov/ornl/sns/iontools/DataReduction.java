971e473e52ec9e00ee5c077894523edae5fba241
Jean Bilheux <bilheuxjm@ornl.gov>
2007-05-21 20:26:54 +0000

    EventHandling code has been moved in its own class. This resolved #168.

--- java/sns-itools-core/src/main/java/gov/ornl/sns/iontools/DataReduction.java
+++ java/sns-itools-core/src/main/java/gov/ornl/sns/iontools/DataReduction.java
@@ -92,7 +92,7 @@ public class DataReduction extends JApplet implements IONDisconnectListener,
     static ExtraPlotInterface myEPinterface;
     static ParametersToKeep   myParametersToKeep;
     
-    DisplayConfiguration    getN;
+    static DisplayConfiguration    getN;
     static float            fNtof;
 
     static ImageIcon[]     	instrumentLogo = new ImageIcon[NUM_LOGO_IMAGES];
@@ -383,7 +383,7 @@ public class DataReduction extends JApplet implements IONDisconnectListener,
    * as well as the temporary folder created for that particular session.
    *
    */
-  private void initializeParameters() {
+  static void initializeParameters() {
 	   
 	  	//retrieve Nx, Ny, NyMin and NyMax according to instrument type
 	    //used to display data
@@ -662,291 +662,9 @@ public class DataReduction extends JApplet implements IONDisconnectListener,
     CheckGUI.populateCheckDataReductionButtonValidationParameters();	
    	CheckGUI.populateCheckDataReductionPlotParameters();
    	liveParameters = new GuiLiveParameters();
-   	   	
-   	if (IParameters.DEBUG) {DebuggingTools.displayData();}
-   	   	
-   	if ("loadctComboBox".equals(evt.getActionCommand())) {
-   		ionLoadct = new IONVariable(loadctComboBox.getSelectedIndex());
-   		String cmd_loadct = "replot_data, " + runNumberValue + ","; 
-		cmd_loadct += ionInstrument + "," + user + ",";
-		cmd_loadct += ionLoadct;
-		cmd_loadct += "," + ionWorkingPathSession;
-		showStatus("Processing...");
-	   	executeCmd(cmd_loadct);
-	   	showStatus("Done!");
-   	}
+   	SuperEventHandler.lookForEvent(evt);
    	
-   	if ("signalPidFileButton".equals(evt.getActionCommand())) {
-		SaveSignalPidFileAction.signalPidFileButton();
-	}
-	
-	if ("clearSignalPidFileButton".equals(evt.getActionCommand())) {
-		SaveSignalPidFileAction.clearSignalPidFileAction();
-		doBox();
-	}
-	
-	if ("backgroundPidFileButton".equals(evt.getActionCommand())) {
-		SaveBackgroundPidFileAction.backgroundPidFileButton();
-	}
-	
-	if ("clearBackPidFileButton".equals(evt.getActionCommand())) {
-		SaveBackgroundPidFileAction.clearBackgroundPidFileAction();
-		doBox();
-	}
-		
-	if ("xValidateButton".equals(evt.getActionCommand()) ||
-		"yValidateButton".equals(evt.getActionCommand()) ||
-		"xMaxTextField".equals(evt.getActionCommand()) ||
-		"xMinTextField".equals(evt.getActionCommand()) ||
-		"yMaxTextField".equals(evt.getActionCommand()) ||
-		"yMinTextField".equals(evt.getActionCommand())) {
-		if (liveParameters.isCombineDataSpectrum()) { //combine plots
-			UpdateDataReductionPlotCombineInterface.validateDataReductionPlotCombineXYAxis();
-		} else { //uncombine plot
-			UpdateDataReductionPlotUncombineInterface.validateDataReductionPlotUncombineXYAxis();
-		}
-	}
-		
-	if ("xResetButton".equals(evt.getActionCommand())) {
-		UpdateDataReductionPlotCombineInterface.resetDataReductionPlotCombineXAxis();
-	}
-	
-	if ("yResetButton".equals(evt.getActionCommand())) {
-		if (liveParameters.isCombineDataSpectrum()) { //combine plots
-			UpdateDataReductionPlotCombineInterface.resetDataReductionPlotCombineYAxis();
-		} else {
-			UpdateDataReductionPlotUncombineInterface.resetDataReductionPlotUncombineYAxis();
-		}
-	}
-	
-	if ("wavelengthMinTextField".equals(evt.getActionCommand())) {
-		WavelengthAction.validatingWavelengthMinText();
-	}
-	
-	if ("wavelengthMaxTextField".equals(evt.getActionCommand())) {
-		WavelengthAction.validatingWavelengthMaxText();
-	}
-	
-	if ("wavelengthWidthTextField".equals(evt.getActionCommand())) {
-		WavelengthAction.validatingWavelengthWidthText();
-	}
-	
-	if ("detectorAngleTextField".equals(evt.getActionCommand())) {
-		DetectorAngleAction.validatingDetectorAngleText();
-	}
-	
-	if ("detectorAnglePMTextField".equals(evt.getActionCommand())) {
-		DetectorAngleAction.validatingDetectorAnglePMText();
-	}
-	
-	if ("yesNormalization".equals(evt.getActionCommand())) {
-	  	NormalizationAction.yesNormalization();
-	}
-
-	if ("noNormalization".equals(evt.getActionCommand())) {
-	   	NormalizationAction.noNormalization();
-	}
-	    
-	if ("normalizationTextField".equals(evt.getActionCommand())) {
-	   	NormalizationAction.yesNormalization();
-	}
-
-	if ("yesBackground".equals(evt.getActionCommand())) {
-		extraPlotsBSCheckBox.setEnabled(true);
-	}
-
-	if ("noBackground".equals(evt.getActionCommand())) {
-		extraPlotsBSCheckBox.setEnabled(false);
-	}
-
-	if ("yesNormBackground".equals(evt.getActionCommand())) {
-		extraPlotsBRNCheckBox.setEnabled(true);
-	}
-
-	if ("noNormBackground".equals(evt.getActionCommand())) {
-		extraPlotsBRNCheckBox.setEnabled(false);
-	}
-
-	if ("yesIntermediate".equals(evt.getActionCommand())) {
-		intermediateButton.setEnabled(true);
-	}
-
-	if ("noIntermediate".equals(evt.getActionCommand())) {
-		intermediateButton.setEnabled(false);
-	}
-
-	if ("intermediateButton".equals(evt.getActionCommand())) {
-		tabbedPane.setSelectedIndex(3);
-	}
-	    
-	/* Settings tab */
-	if ("settingsValidateButton".equals(evt.getActionCommand())) {
-		tabbedPane.setSelectedIndex(0);
-		dataReductionTabbedPane.setSelectedIndex(0);	
-	}
-	
-	if ("settingsSelectAllButton".equals(evt.getActionCommand())) {
-		SettingsPanelAction.selectAllSettingsTab();
-	}
-	
-	if ("settingsUnselectAllButton".equals(evt.getActionCommand())) {
-		SettingsPanelAction.unselectAllSettingsTab();
-	}
-	
-	if ("yesCombineSpectrum".equals(evt.getActionCommand())) {
-	   	CheckDataReductionButtonValidation.bCombineDataSpectrum = true;
-	   	CheckGUI.enableDataReductionPlotXAxis();
-	}
-
-	if ("noCombineSpectrum".equals(evt.getActionCommand())) {
-	   	if (DataReduction.instrument.compareTo(IParameters.REF_M)==0) { //REF_M
-	   		DataReduction.yesCombineSpectrumRadioButton.setSelected(true);
-	   	} else {
-	   		CheckDataReductionButtonValidation.bCombineDataSpectrum = false;
-	   	}
-		CheckGUI.disableDataReductionPlotXAxis();
-	}
-	    
-	if ("yesInstrumentGeometry".equals(evt.getActionCommand())) {
-	   	InstrumentGeometryAction.yesInstrumentGeometry();
-	}
-
-	if ("noInstrumentGeometry".equals(evt.getActionCommand())) {
-	   	InstrumentGeometryAction.noInstrumentGeometry();
-	}
-
-	if ("instrumentGeometryButton".equals(evt.getActionCommand())) {
-	}
-
-	if ("instrumentGeometryTextField".equals(evt.getActionCommand())) {
-	   	InstrumentGeometryAction.yesInstrumentGeometry();	    		
-	}
-
-	if ("startDataReductionButton".equals(evt.getActionCommand())) {
-	    	
-		String cmd_local = CreateCmdAndRunDataReduction.createDataReductionCmd();
-	  	cmd_local += ExtraPlots.createIDLcmd();  //add extra plot
-	    
-		//main plot run in another thread
-		SubmitDataReduction run = new SubmitDataReduction(cmd_local);
-		Thread runThread = new Thread(run,"Data Reduction in progess");
-		runThread.start();
-
-	}
-	    
-	//if one of the intermediate check box is check
-	if ("plot1".equals(evt.getActionCommand())) {
-	}
-
-	if ("instrumentREFL".equals(evt.getActionCommand())) {
-	   	displayInstrumentLogo(0);
-	  	instrument = IParameters.REF_L;
-	  	CheckGUI.populateCheckDataReductionButtonValidationParameters();
-	  	c_plot_REFL.setVisible(true);
-	  	c_plot_REFM.setVisible(false);
-	  	c_plot = c_plot_REFL;
-	  	UpdateMainGUI.prepareGUI();
-	  	initializeParameters();
-	}
-
-	if ("instrumentREFM".equals(evt.getActionCommand())) {
-	   	displayInstrumentLogo(1);
-	   	instrument = IParameters.REF_M;
-	   	CheckGUI.populateCheckDataReductionButtonValidationParameters();
-	   	c_plot_REFL.setVisible(false);
-	  	c_plot_REFM.setVisible(true);
-	  	c_plot = c_plot_REFM;
-	  	UpdateMainGUI.prepareGUI();
-	  	initializeParameters();
-	}
-
-	if ("preferencesMenuItem".equals(evt.getActionCommand())) {
-	//    preferencesFrame.setVisible(true);
-	}
-
-	if ("signalSelection".equals(evt.getActionCommand())) {
-		modeSelected = "signalSelection";
-	}
-	    
-	if ("back1Selection".equals(evt.getActionCommand())) {
-		modeSelected = "back1Selection";
-	}
-	    
-	if ("back2Selection".equals(evt.getActionCommand())) {
-		modeSelected = "back2Selection";
-	}
-	    
-	if ("info".equals(evt.getActionCommand())) {
-		modeSelected = "info";
-	}
-	    
-	if ("runsAddTextField".equals(evt.getActionCommand())) {
-	}
-	    
-	if ("runsSequenceTextField".equals(evt.getActionCommand())) {
-	}
-
-	if ("runNumberTextField".equals(evt.getActionCommand())) {
-	    
-		//reset color of all tabs
-		TabUtils.removedAllTabColor();
-		
-		//retrive value of run number
-		runNumberValue = runNumberTextField.getText();
-		
-		if (runNumberValue.compareTo("") != 0) {   //plot only if there is a run number
-	
-			//remove or not parameters we don't want anymore
-			ParametersToKeep.refreshGuiWithParametersToKeep();
-						/*
-		    //retrieve name of instrument
-		    instrument = (String)instrList.getSelectedItem();
-		    */
-
-			ionInstrument = new com.rsi.ion.IONVariable(instrument);
-			user = new com.rsi.ion.IONVariable(ucams); 
-			ionLoadct = new IONVariable(loadctComboBox.getSelectedIndex());
-			ionWorkingPathSession = new IONVariable(ParametersToKeep.sSessionWorkingDirectory);
-			
-			c_ionCon.setDrawable(c_plot);
-	    		    	
-			String cmd = "result = plot_data( " + runNumberValue + ", " + 
-			ionInstrument + ", " + user + "," + ionLoadct;
-			cmd += "," + ionWorkingPathSession + ")";
-
-			//main plot run in another thread
-			SubmitPlot run = new SubmitPlot(cmd);
-			Thread runThread = new Thread(run,"plot in progress");
-			runThread.start();
-			
-		}	
-	}		
-
-	if ("xValidateButtonEP".equals(evt.getActionCommand()) || 
-		"yValidateButtonEP".equals(evt.getActionCommand()) ||
-		"xMinTextFieldEP".equals(evt.getActionCommand()) ||
-		"xMaxTextFieldEP".equals(evt.getActionCommand()) ||
-		"yMinTextFieldEP".equals(evt.getActionCommand()) ||
-		"yMaxTextFieldEP".equals(evt.getActionCommand())) {
-		bEPFirstTime = false;
-		UpdateExtraPlotsInterface.updateExtraPlotsGUI();
-	}
-	
-	if ("xResetButtonEP".equals(evt.getActionCommand()) ||
-	    "yResetButtonEP".equals(evt.getActionCommand())) {	
-		UpdateExtraPlotsInterface.refreshExtraPlotsGUI();
-	}
-		
-	if (CheckDataReductionButtonValidation.checkDataReductionButtonStatus()) {
-		startDataReductionButton.setEnabled(true);
-	} else {
-		startDataReductionButton.setEnabled(false);
-	}
-
-	if ("replotSelectionButton".equals(evt.getActionCommand())) {
-		doBox();
-	}
-
+   	
 	liveParameters = new GuiLiveParameters();
 	
     }   
@@ -1235,7 +953,7 @@ public class DataReduction extends JApplet implements IONDisconnectListener,
 	panel2.add(scrollPane);
     }
 
-    private void displayInstrumentLogo(int instrumentIndex) {
+    static void displayInstrumentLogo(int instrumentIndex) {
     	instrumentLogoLabel.setIcon(instrumentLogo[instrumentIndex]);		
     }
     
