6e348930fa27fc55795e34731474c1e7567d8554
j35 <j35@93334afa-840f-0410-a8fc-81e7bb264a29>
2009-02-26 21:22:00 +0000

    Added new tag. This resolves #2178

---
+++ tags/SANScalibration/1.1.1/make_tar.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+file_name_to_search=$1
+list_of_files_folders="sans_calibration.sav SANScalibration README install_SANScalibration images drversion SANScalibration.cfg"
+
+application=`./get_application.pl $file_name_to_search`
+version=`./get_version.pl $file_name_to_search`
+
+application="SANScalibration"
+version="1.1.1"
+
+tar_file_name="./"$application"_"$version".tar"
+#create tar file
+tar cvf $tar_file_name $list_of_files_folders
+
